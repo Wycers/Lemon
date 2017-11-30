013f5c607addb4680773e5f20c4f361e83a22c32
Wycer <734849165@qq.com>
2017-12-01 02:11:11 +0800

    login fix

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,22 +1,23 @@
 <template lang="pug">
 div
-  v-dialog(:value='true', max-width="290", persistent='')
-    v-card(hover='', style='background:white')
-      v-card-row.deep-purple.darken-1
-        v-card-title.black--text
-          div(style="font-size: x-large",class="text-xs-center")  {{$t("Login")}}
-      v-card-row
-        v-card-text.pt-4
-          v-form(v-model='model', action='login', @success='onSuccess', submitButtonText="Login")
-            v-text-field(:label="`${$t('Username')}`", v-model="model.username", required, type="text")
-            v-text-field(:label="`${$t('Password')}`", v-model="model.password", required, type="password")
-            .flex.pb-2
-              small {{$t("* Indicates required field")}}
-            
+  v-app.main
+    v-dialog(:value='true', max-width="290", persistent='')
+      v-card(hover='', style='background:white')
+        v-card-row.deep-purple.darken-1
+          v-card-title.black--text
+            div(style="font-size: x-large",class="text-xs-center")  {{$t("Login")}}
+        v-card-row
+          v-card-text.pt-4
+            v-form(v-model='model', :fields='fields' action='login', @success='onSuccess', submitButtonText="Login")
+              v-text-field(:label="`${$t('Username')}`", v-model="model.username", required, type="text")
+              v-text-field(:label="`${$t('Password')}`", v-model="model.password", required, type="password")
+              .flex.pb-2
+                small {{$t("* Indicates required field")}}
+              
 </template>
 
 <style>
-  body{
+  .main{
     background: url(http://api.dujin.org/bing/1366.php) no-repeat center;
   }
 </style>
